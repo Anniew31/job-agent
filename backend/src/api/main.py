d7e9d68bb27a087b016206e0424b7584c52bc463
Annie Weng <126124914+Anniew31@users.noreply.github.com>
2026-06-20 21:31:10 -0400

    fix cors url

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -17,7 +17,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://job-agent-pipeline.vercel.app/"],
+    allow_origins=["http://localhost:3000", "https://job-agent-pipeline.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
